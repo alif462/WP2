e8c954bd14a2c29fadd63f7b2d615ada1de5a6ad
alif462 <mualifdwisaputra285@gmail.com>
2020-11-04 08:42:58 +0700

    Pustaka

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Luthfil Khakim</li>
+        <li>Nama Belakang : Khakim</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Adiwerna, Tegal </li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Tegal</li></ul>
+
+    <h4>Favorit</h4>
+    <ul type="square">
+        <li>Bersantai-santai</li>
+        
+    </ul>
+</section>
